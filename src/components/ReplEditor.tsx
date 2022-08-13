207a264a803d4a54d51254db11cf367b00e1f4f4
Lordson Vimal <ylordson@gmail.com>
2022-08-13 14:05:09 +0530

    WIP: editor and console

--- src/components/ReplEditor.tsx
+++ src/components/ReplEditor.tsx
@@ -1,22 +1,53 @@
-import { onMount } from "solid-js"
+import { createSignal, For, onMount } from "solid-js"
 import { basicSetup, EditorView } from "codemirror"
-import { javascript } from "@codemirror/lang-javascript"
+import { ViewUpdate } from "@codemirror/view"
 
 import "../styles/ReplEditor.scss"
 
 function ReplEditor() {
+  const [log, setLog] = createSignal(["test", "new"])
   
-  const editorParent = <div class="editorParent"></div>
+  const editorParent = <div class="editorParent"></div> as Element
+
+  const updateLitener = (view: ViewUpdate) => {
+    if (view.docChanged) {
+      // TODO: Parse and evaluate, and update log
+      // setProgram(view.state.doc.toString())
+    }
+  }
 
   onMount(() => {
+    const defaultProgram = `def fib(n) [
+  if (n <= 1) ret 1;
+  ret fib(n - 1) + fib(n - 2);
+]
+fibonacci(5);\n`
+
     new EditorView({
-      doc: "console.log('hello')\n",
-      extensions: [basicSetup, javascript()],
+      doc: defaultProgram,
+      extensions: [
+        basicSetup,
+        EditorView.updateListener.of(updateLitener)
+      ],
       parent: editorParent
     })
+
   })
 
-  return <section class="editorContainer">{editorParent}</section>;
+  return (
+    <section class="editorContainer">
+      <div class="buttons">
+        <button onClick={() => setLog([...log(), Math.random().toString()])}>Run</button>
+        <button onClick={() => setLog([])}>Clear</button>
+      </div>
+      {editorParent}
+      <section class="editorConsole">
+        <For each={log()}>
+          {item => <div>{item}</div>}
+        </For>
+      </section>
+    </section>
+  )
 }
 
 export default ReplEditor
